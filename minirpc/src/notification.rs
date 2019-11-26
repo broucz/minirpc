164e463ff1ce8e4aa73049e7f0f86f67a81d1a57
broucz <broucapierre@gmail.com>
2019-11-26 19:01:43 +0100

    Generic types parameters

--- minirpc/src/notification.rs
+++ minirpc/src/notification.rs
@@ -1,40 +1,41 @@
 //! MINI-RPC Notification Request.
 
-use crate::{Method, Params};
-
 /// Represents a MINI-RPC Request which is a notification.
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
-pub struct Notification {
+pub struct Notification<M, P> {
     /// A String containing the name of the method to be invoked.
-    pub method: Method,
+    pub method: M,
 
     /// A Structured value that holds the parameter values to be used during the invocation of the method.
-    pub params: Params,
+    pub params: P,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::{self, Value};
+    use serde_json;
+
+    type M = String;
+    type P = Vec<u64>;
 
     #[test]
     fn notification_deserialization() {
         let input = r#"{"method":"test_method","params":[1,2,3]}"#;
         let expected = Notification {
-            method: Method::String("test_method".to_owned()),
-            params: Params::Array(vec![Value::from(1), Value::from(2), Value::from(3)]),
+            method: "test_method".to_owned(),
+            params: vec![1, 2, 3],
         };
 
-        let result: Notification = serde_json::from_str(input).unwrap();
+        let result: Notification<M, P> = serde_json::from_str(input).unwrap();
         assert_eq!(result, expected);
     }
 
     #[test]
     fn notification_serialization() {
         let input = Notification {
-            method: Method::String("test_method".to_owned()),
-            params: Params::Array(vec![Value::from(1), Value::from(2), Value::from(3)]),
+            method: "test_method".to_owned(),
+            params: vec![1, 2, 3],
         };
         let expected = r#"{"method":"test_method","params":[1,2,3]}"#;
 
